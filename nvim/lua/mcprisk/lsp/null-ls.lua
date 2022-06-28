233f2d6e4365df9ab6c40d09838486be550e7d2c
Matt Priskorn <mcp@cygnus-x1.com>
2022-06-28 22:48:12 +0200

    Added python lsp server and formatter

--- nvim/lua/mcprisk/lsp/null-ls.lua
+++ nvim/lua/mcprisk/lsp/null-ls.lua
@@ -11,5 +11,6 @@ null_ls.setup({
     debug = false,
     sources = {
         formatting.stylua.with({ extra_args = { "--search-parent-directories", }, }),
+        formatting.black.with({ extra_args = { "--fast", }, }),
     },
 })
